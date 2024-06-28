85c3f6ed772dd68600c0ee0f8019af1b23030e94
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-06-28 12:56:30 +0200

    Added about and contact pages

---
+++ components/About.tsx
@@ -0,0 +1,27 @@
+const About = () => {
+	return (
+		<section className="text-white flex h-full items-center justify-center">
+			<div className="flex flex-col w-full h-full justify-start items-center text-center sm:text-left bg-green-900">
+				<h1 className="text-3xl font-semibold py-10 mt-20 font-poppins">
+					About
+				</h1>
+				<p className="text-lg leading-6 max-w-prose px-5">
+					<span className="text-green-400">Flori</span>
+					<span className="font-bold">Hub</span> is a platform for all
+					matter of flora, goal of this project is to let users learn
+					about different plants with general information regarding
+					what it is, what it looks like, and what are its care
+					requirements.
+				</p>
+				<p className="text-lg leading-6 max-w-prose mt-10 px-5 pb-5">
+					This service allow users to create accounts which they can
+					use to add existing plants on the platform to their
+					collection, then the user can use that as a tracker of their
+					plants growth by adding images with timestamps every X time,
+					tracking last time watered etc.
+				</p>
+			</div>
+		</section>
+	);
+};
+export default About;
---
+++ components/Contact.tsx
@@ -0,0 +1,34 @@
+import { FaGithub, FaEnvelope } from "react-icons/fa";
+
+const Contact = () => {
+	return (
+		<section className="text-white flex h-full items-center justify-center">
+			<div className="flex flex-col w-full h-full text-center justify-start bg-green-900">
+				<h1 className="text-3xl font-semibold py-10 mt-20 font-poppins">
+					Contact
+				</h1>
+				<p className="px-5 text-lg">
+					<span className="text-green-400">Flori</span>
+					<span className="font-bold">Hub</span> was created by a
+					single person, you can contact them by clicking on the mail
+					icon below or through github.
+				</p>
+				<div className="flex gap-12 p-10 justify-center items-center">
+					<a href="mailto:przemyslaw.kaczmarski11@gmail.com">
+						<FaEnvelope
+							title="email"
+							className="text-3xl hover:cursor-pointer"
+						/>
+					</a>
+					<a href="https://github.com/Nyviel">
+						<FaGithub
+							title="Github"
+							className="text-3xl hover:cursor-pointer"
+						/>
+					</a>
+				</div>
+			</div>
+		</section>
+	);
+};
+export default Contact;
