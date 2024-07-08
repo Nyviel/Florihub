b36a9bf36b25899b15f50fbab0dddfc281c677a2
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-07-08 13:46:27 +0200

    Added spinner

---
+++ app/loading.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { ClipLoader } from "react-spinners";
+const override = {
+	display: "block",
+	position: "absolute",
+	top: "50%",
+	left: "50%",
+	translate: "-50% -50%",
+};
+
+const LoadingPage = ({ loading }: { loading: boolean }) => {
+	return (
+		<ClipLoader
+			color="#39ff14"
+			loading={loading}
+			cssOverride={override}
+			size={150}
+			aria-label="Loading spinner"
+		/>
+	);
+};
+
+export default LoadingPage;
