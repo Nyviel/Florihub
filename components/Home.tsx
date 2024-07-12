aae270420311774bad4efca1d8b84010694220af
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-07-12 08:47:56 +0200

    Reworked home page

--- components/Home.tsx
+++ components/Home.tsx
@@ -81,72 +81,56 @@ const Home = () => {
 	};
 
 	return (
-		<section className="text-white flex flex-col md:flex-row h-screen items-center justify-center">
-			<div className="flex flex-col w-full h-full text-center justify-center bg-green-900">
-				<div className="flex flex-col justify-center items-center h-1/4 w-full -mt-20">
-					<div className="h-24">
-						<h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold">
-							Welcome to{" "}
-							<span className="text-green-400">Flori</span>
-							<span className="font-bold">Hub</span>
-						</h1>
-					</div>
-					<div className="flex-1 flex justify-center items-center">
-						<div className=" flex justify-center items-center gap-2 text-2xl p-2">
-							Your go to hub for
-							<span className="text-green-400">
-								<Typewriter
-									options={{
-										strings: words,
-										delay: 75,
-										autoStart: true,
-										loop: true,
-									}}
-								/>
-							</span>
-						</div>
-					</div>
+		<section className="flex flex-col md:flex-row h-full">
+			<div className="w-full h-full flex flex-col items-center justify-center text-white bg-green-700">
+				<h1 className="text-xl md:text-2xl lg:text-3xl lg:leading-10">
+					Welcome to <span className="text-green-500">Flori</span>Hub
+				</h1>
+				<div className="flex justify-center items-center gap-2 text-xl md:text-2xl lg:text-3xl lg:leading-10 p-2">
+					Your go to hub for
+					<span className="text-green-400">
+						<Typewriter
+							options={{
+								strings: words,
+								delay: 75,
+								autoStart: true,
+								loop: true,
+							}}
+						/>
+					</span>
 				</div>
 			</div>
-			<div className="flex flex-col w-full h-full justify-center text-center bg-gray-900">
-				<div className="flex flex-col justify-center items-center h-1/4 w-full -mt-20">
-					<div className="h-fit sm:h-24">
-						<h2 className="text-xl md:text-2xl lg:text-3xl lg:leading-10">
-							Learn something new today, <br />
-							select a plant from the form below
-						</h2>
-					</div>
-					<div className="flex-1 w-full py-4">
-						<form className="flex flex-col gap-6 justify-center items-center w-full">
-							<Autocomplete
-								value={value}
-								onChange={(
-									event: any,
-									newValue: string | null
-								) => {
-									setValue(newValue);
-								}}
-								id="combo-box-demo"
-								options={plantTitles}
-								sx={{ width: 300 }}
-								renderInput={(params) => (
-									<TextField
-										{...params}
-										label="Enter plant name..."
-										sx={inputFieldStyling}
-									/>
-								)}
+			<div className="w-full h-full flex flex-col items-center justify-center text-white text-center bg-gray-900">
+				<h2 className="text-xl md:text-2xl lg:text-3xl lg:leading-10">
+					Learn something new today, <br />
+					select a plant from the form below
+				</h2>
+
+				<form className="flex flex-col gap-6 justify-center items-center w-full p-4">
+					<Autocomplete
+						value={value}
+						onChange={(event: any, newValue: string | null) => {
+							setValue(newValue);
+						}}
+						id="combo-box-demo"
+						options={plantTitles}
+						sx={{ width: 300 }}
+						renderInput={(params) => (
+							<TextField
+								{...params}
+								label="Enter plant name..."
+								sx={inputFieldStyling}
 							/>
-							<button
-								type="button"
-								onClick={handleFormSubmit}
-								className="px-4 py-2 bg-green-700 rounded"
-							>
-								Search
-							</button>
-						</form>
-					</div>
-				</div>
+						)}
+					/>
+					<button
+						type="button"
+						onClick={handleFormSubmit}
+						className="px-4 py-2 bg-green-700 rounded"
+					>
+						Search
+					</button>
+				</form>
 			</div>
 		</section>
 	);
