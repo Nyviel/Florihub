da1182525b3c079fecdb35f78f450075a9d1f6eb
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-07-12 08:48:53 +0200

    Added Spinner component

---
+++ components/Spinner.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { ClipLoader } from "react-spinners";
+const override = {
+	display: "block",
+	margin: "100px auto",
+};
+const Spinner = ({ loading }: { loading: boolean }) => {
+	return (
+		<div className="min-h-full">
+			<ClipLoader
+				color="#3b82f6"
+				loading={loading}
+				cssOverride={override}
+				size={150}
+				aria-label="Loading spinner"
+			/>
+		</div>
+	);
+};
+
+export default Spinner;
