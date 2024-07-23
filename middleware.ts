1a1411686af4bc25edcff8250d4c82e0ab38e5ca
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-07-23 17:40:36 +0200

    Added auth guard for /profile route

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,15 @@ import { withAuth } from "next-auth/middleware";
 
 export default withAuth(function middleware(_) {}, {
 	callbacks: {
-		authorized: ({ token }) => token?.isAdmin === true,
+		authorized: ({ token, req }) => {
+			console.log("URL:", req.nextUrl);
+			if (req.nextUrl.pathname === "/dashboard") {
+				return token?.isAdmin === true;
+			}
+
+			return !!token;
+		},
 	},
 });
 
-export const config = { matcher: ["/dashboard"] };
+export const config = { matcher: ["/dashboard", "/profile"] };
