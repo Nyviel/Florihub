2cabcbad53e4da67b28cfa5447c80dbbe65a659b
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-07-29 14:03:57 +0200

    Changed types of front interfaces

--- interfaces/trackedPlant.ts
+++ interfaces/trackedPlant.ts
@@ -1,14 +1,16 @@
 type TimelineEntryEvent = "water" | "image" | null;
 
 export interface TimelineEntry {
-	date: Number;
-	value: String;
+	date: number;
+	value: string;
 	event: TimelineEntryEvent;
 }
 
 export interface TrackedPlant {
+	plantId: string;
 	userId: string;
 	name: string;
 	timeline: TimelineEntry[];
 	images: string[];
+	thumbnail: string;
 }
