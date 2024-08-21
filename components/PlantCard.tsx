fda6eae22b1e05a9e10008887d9711089ceb43de
Przemysław Kaczmarski <nyviel111@gmail.com>
2024-08-21 12:04:25 +0200

    CSS changes to plant cards

--- components/PlantCard.tsx
+++ components/PlantCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Plant } from "@/interfaces/plant";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const PlantCard = ({ plant }: { plant: Plant }) => {
@@ -8,23 +9,37 @@ const PlantCard = ({ plant }: { plant: Plant }) => {
 		router.push(`/details?plant=${plant.name}`);
 	};
 	return (
-		<div className="w-full h-fit max-h-full bg-gray-900 rounded-lg space-y-4 border border-gray-950 shadow-md shadow-black">
-			<div className="w-full h-2/5">
-				<img
+		<div className="w-full mx-auto h-full bg-gray-900 rounded-lg space-y-4 border border-gray-950 shadow-md shadow-black">
+			<div className="w-full h-[200px] md:h-[400px] overflow-hidden">
+				<Image
 					src={`/images/${plant.thumbnail}`}
 					alt="Plant thumbnail"
-					className="w-full h-full bg-cover bg-center rounded-t-lg border border-gray-950"
+					width={0}
+					height={0}
+					sizes="100vw"
+					className="w-full h-full object-cover object-center border rounded-t-lg border-gray-950"
 				/>
 			</div>
-			<div className="w-full flex flex-col justify-center items-center">
-				<h1 className="text-2xl">{plant.name}</h1>
-				<p className="text-lg text-gray-300 p-2 px-4 text-center">
-					{plant.description.substring(0, 100)}...
+			<div className="w-full flex flex-col justify-center items-center gap-2">
+				<h1 className="text-lg md:text-2xl text-center font-semibold">
+					{plant.name}
+				</h1>
+				<p className="text-base md:text-lg text-gray-300 p-2 px-4 text-center">
+					{plant.description.substring(
+						0,
+						plant.description.length > 200
+							? 200
+							: plant.description.length
+					)}
+					...
 				</p>
 			</div>
 			<div className="text-gray-100 flex justify-center gap-2 flex-wrap px-6">
 				{plant?.tags.map((tag) => (
-					<p key={tag} className="bg-green-800 rounded-md p-2 h-fit">
+					<p
+						key={tag}
+						className="bg-green-800 rounded-md p-2 h-fit text-center"
+					>
 						{tag}
 					</p>
 				))}
--- components/PlantCards.tsx
+++ components/PlantCards.tsx
@@ -1,16 +1,30 @@
 import { Plant } from "@/interfaces/plant";
 import PlantCard from "./PlantCard";
 import { useEffect } from "react";
+import { cn } from "@/lib/utils";
 
 const PlantCards = ({ plants }: { plants: Plant[] }) => {
 	useEffect(() => {
 		console.log(plants);
 	}, [plants]);
 	return (
-		<div className="h-full w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-			{plants?.map((plant) => {
-				return <PlantCard key={plant._id} plant={plant} />;
-			})}
+		<div
+			className={cn(
+				"h-full w-full grid grid-cols-1 gap-4",
+				plants.length <= 1
+					? "md:grid-cols-1"
+					: plants.length <= 2
+					? "md:grid-cols-2"
+					: "md:grid-cols-3"
+			)}
+		>
+			{plants.length ? (
+				plants?.map((plant) => {
+					return <PlantCard key={plant._id} plant={plant} />;
+				})
+			) : (
+				<h2 className="text-2xl text-center">No results found</h2>
+			)}
 		</div>
 	);
 };
